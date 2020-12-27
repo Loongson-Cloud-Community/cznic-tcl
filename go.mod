0bfd8c5597384a4eab51ef77e4e05d9f5ff9af38
Jan Mercl <0xjnml@gmail.com>
2020-12-27 23:19:59 +0100

    bump libc version

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.15
 
 require (
 	modernc.org/httpfs v1.0.2
-	modernc.org/libc v0.0.0-20201226183002-ad4fe8567bad
+	modernc.org/libc v0.0.0-20201227215836-b48462471824
 )
--- lib/patch_darwin_amd64.go
+++ lib/patch_darwin_amd64.go
@@ -7,14 +7,14 @@ package tcl
 import (
 	"fmt"
 	"os"
-	// "os/exec"
+	"os/exec"
 	"runtime"
 	"strings"
-	// "syscall"
-	// "unsafe"
+	"syscall"
+	"unsafe"
 
 	"modernc.org/libc"
-	// "modernc.org/libc/errno"
+	"modernc.org/libc/errno"
 )
 
 func todo(s string, args ...interface{}) string { //TODO-
@@ -56,54 +56,53 @@ func trc(s string, args ...interface{}) string { //TODO-
 var createProcessMsg = [...]byte{'%', 's', ':', ' ', '%', 's', 0}
 
 func XTclpCreateProcess(tls *libc.TLS, interp uintptr, argc int32, argv uintptr, inputFile, outputFile, errorFile, pidPtr uintptr) int32 { /* tclUnixPipe.c:379:1: */
-	panic(todo(""))
-	// bp := tls.Alloc(2 * 8)
-	// defer tls.Free(2 * 8)
-	// var args []string
-	// for i := 0; i < int(argc); i++ {
-	// 	p := *(*uintptr)(unsafe.Pointer(argv + unsafe.Sizeof(uintptr(0))*uintptr(i)))
-	// 	args = append(args, libc.GoString(p))
-	// }
-	// if len(args) == 0 {
-	// 	panic(todo(""))
-	// }
+	bp := tls.Alloc(2 * 8)
+	defer tls.Free(2 * 8)
+	var args []string
+	for i := 0; i < int(argc); i++ {
+		p := *(*uintptr)(unsafe.Pointer(argv + unsafe.Sizeof(uintptr(0))*uintptr(i)))
+		args = append(args, libc.GoString(p))
+	}
+	if len(args) == 0 {
+		panic(todo(""))
+	}
 
-	// args0, err := exec.LookPath(args[0])
-	// if err != nil {
-	// 	*(*int32)(unsafe.Pointer(libc.X__errno_location(tls))) = errno.ENOENT
-	// 	s, err := libc.CString(fmt.Sprintf("couldn't execute \"%.150s\"", args[0]))
-	// 	if err != nil {
-	// 		panic(todo(""))
-	// 	}
+	args0, err := exec.LookPath(args[0])
+	if err != nil {
+		*(*int32)(unsafe.Pointer(libc.X__errno_location(tls))) = errno.ENOENT
+		s, err := libc.CString(fmt.Sprintf("couldn't execute \"%.150s\"", args[0]))
+		if err != nil {
+			panic(todo(""))
+		}
 
-	// 	defer libc.Xfree(tls, s)
+		defer libc.Xfree(tls, s)
 
-	// 	XTcl_SetObjResult(tls, interp, XTcl_ObjPrintf(tls, uintptr(unsafe.Pointer(&createProcessMsg[0])), libc.VaList(bp, s, XTcl_PosixError(tls, interp))))
-	// 	return TCL_ERROR
-	// }
+		XTcl_SetObjResult(tls, interp, XTcl_ObjPrintf(tls, uintptr(unsafe.Pointer(&createProcessMsg[0])), libc.VaList(bp, s, XTcl_PosixError(tls, interp))))
+		return TCL_ERROR
+	}
 
-	// args[0] = args0
-	// env := libc.GetEnviron()
-	// attr := &syscall.ProcAttr{
-	// 	Env:   env,
-	// 	Files: []uintptr{^uintptr(0), ^uintptr(0), ^uintptr(0)},
-	// }
-	// if inputFile != 0 {
-	// 	attr.Files[syscall.Stdin] = inputFile - 1 // tclUnixPipe.c:27: #define GetFd(file) (PTR2INT(file) - 1)
-	// }
-	// if outputFile != 0 {
-	// 	attr.Files[syscall.Stdout] = outputFile - 1
-	// }
-	// if errorFile != 0 {
-	// 	attr.Files[syscall.Stderr] = errorFile - 1
-	// }
-	// pid, err := syscall.ForkExec(args0, args, attr)
-	// if err != nil {
-	// 	trc("TclpCreateProcess(%#x, %d, %q, %v, %v, %v, %#x): %v", interp, len(args), args, inputFile, outputFile, errorFile, pidPtr, err)
-	// 	panic(todo(""))
-	// }
+	args[0] = args0
+	env := libc.GetEnviron()
+	attr := &syscall.ProcAttr{
+		Env:   env,
+		Files: []uintptr{^uintptr(0), ^uintptr(0), ^uintptr(0)},
+	}
+	if inputFile != 0 {
+		attr.Files[syscall.Stdin] = inputFile - 1 // tclUnixPipe.c:27: #define GetFd(file) (PTR2INT(file) - 1)
+	}
+	if outputFile != 0 {
+		attr.Files[syscall.Stdout] = outputFile - 1
+	}
+	if errorFile != 0 {
+		attr.Files[syscall.Stderr] = errorFile - 1
+	}
+	pid, err := syscall.ForkExec(args0, args, attr)
+	if err != nil {
+		trc("TclpCreateProcess(%#x, %d, %q, %v, %v, %v, %#x): %v", interp, len(args), args, inputFile, outputFile, errorFile, pidPtr, err)
+		panic(todo(""))
+	}
 
-	// // trc("TclpCreateProcess(%#x, %d, %q, %v, %v, %v, %#x): pid %v", interp, len(args), args, inputFile, outputFile, errorFile, pidPtr, pid)
-	// *(*uintptr)(unsafe.Pointer(pidPtr)) = uintptr(pid)
-	// return TCL_OK
+	// trc("TclpCreateProcess(%#x, %d, %q, %v, %v, %v, %#x): pid %v", interp, len(args), args, inputFile, outputFile, errorFile, pidPtr, pid)
+	*(*uintptr)(unsafe.Pointer(pidPtr)) = uintptr(pid)
+	return TCL_OK
 }
